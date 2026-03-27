a36ef8c4e23f1698dd82ccfee7a70e87c6da71fe
cbituin02 <cbituin02@gmail.com>
2026-03-26 17:06:28 -0700

    3/26 5:06

--- FinalProject_CalebBituin/src/project/Dog.java
+++ FinalProject_CalebBituin/src/project/Dog.java
@@ -2,7 +2,12 @@ package project;
 
 import java.util.Scanner;
 import java.util.Random;
-
+/*
+ * TO DO:
+ * ORGANIZE CODE LIKE THE PLAY METHOD
+ * (HUNGER WITH HUNGER)
+ * (ENERGY WITH ENERGY)
+ */
 public class Dog {
 
 	// --------------- FIELDS --------------- //
@@ -16,15 +21,13 @@ public class Dog {
 	private static int hunger = 50;
 	private int updateHunger;
 	private int newHunger;
-	private int previousHunger;
-	private int resultPlayHunger;
 	
-	private static int energy = 25;
+	private static int energy = 50;
 	private int updateEnergy;
 	private int energyIndex;
 	private int newEnergy;
-	private int previousEnergy;
-	private int resultPlayEnergy;
+	
+	private int sleepIndex;
 	
 	// --------------- CONSTRUCTOR --------------- //
 	public Dog(Scanner keyboard) {
@@ -38,14 +41,14 @@ public class Dog {
 		int[] dogAge = {1, 2, 3, 4};
 		
 		// random number
-		Random random = new Random();
+		Random randomAge = new Random();
 		
 		// get random number from the age array
-		int randomAgeIndex = random.nextInt(dogAge.length);
+		int randomAgeIndex = randomAge.nextInt(dogAge.length);
 		
 		// get the random age and set it
-		int randomAge = dogAge[randomAgeIndex];
-		age = randomAge;
+		int randomAgeValue = dogAge[randomAgeIndex];
+		age = randomAgeValue;
 	}
 	
 	// --------------- METHODS --------------- //
@@ -69,7 +72,15 @@ public class Dog {
 		int randomHunger = dogHunger[randomHungerIndex];
 		
 		// set update value to default
-		this.updateHunger = hunger;
+		if (hunger >= maxTotal) {
+			hunger = maxTotal;
+			this.updateHunger = hunger;
+		} else if (hunger <= minTotal) {
+			hunger = minTotal;
+			this.updateHunger = hunger;
+		} else {
+			this.updateHunger = hunger;
+		}
 		
 		// set previous to old value
 		int previousHunger = this.updateHunger;
@@ -86,6 +97,7 @@ public class Dog {
 		// create energy array
 		int[] dogEnergy = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 		
+		// let
 		if (randomHunger == dogHunger[0]) {
 			energyIndex = dogEnergy[0];
 		} else if ( randomHunger == dogHunger[1]) {
@@ -111,10 +123,17 @@ public class Dog {
 		} else if ( randomHunger == dogHunger[11]) {
 			energyIndex = dogEnergy[11];
 		}
-		
 			
 		// set the updateValue to default
-		this.updateEnergy = energy;
+		if (energy <= minTotal) {
+			energy = minTotal;
+			this.updateEnergy = energy;
+		} else if (energy >= maxTotal) {
+			energy = maxTotal;
+			this.updateEnergy = energy;
+		} else {
+			this.updateEnergy = energy;
+		}
 		
 		// previous value
 		int previousEnergy = this.updateEnergy;
@@ -149,73 +168,249 @@ public class Dog {
 			this.updateEnergy = resultPlayEnergy;
 			System.out.println("Hunger Level: " + previousHunger + ". Energy Level: " + this.updateEnergy);
 		} else {
-			System.out.println("Hunger Level: " + this.updateHunger + ". Energy Level: " + energy);
+			System.out.println("Hunger Level: " + hunger + ". Energy Level: " + energy);
 		}
-				
 	}
 	
 	public void feed() {
-		// when user clicks feed, hunger will decrease by 15 each click
-		// energy will increase by 12
 		
-		// set updateValues to previous values
-		this.updateHunger = hunger;
-		this.updateEnergy = energy;
+		// --------------- HUNGER --------------- //
+
+		// create random hunger array
+		int[] feedHunger = {12, 9, 5, 20, 7, 18, 14};
+		
+		// create random object
+		Random randomFeedHunger = new Random();
+		
+		// create variable that grabs a random # from the feed array
+		int randomFeedHungerIndex = randomFeedHunger.nextInt(feedHunger.length);
+		
+		// create variable to equal the value of random #
+		int randomFeedHungerValue = feedHunger[randomFeedHungerIndex];
+		
+		// if previous values = maxTotal, set previous values to maxTotal, otherwise set previous values
+		if (hunger >= maxTotal) {
+			hunger = maxTotal;
+			this.updateHunger = hunger;
+		} else if (hunger <= minTotal) {
+			hunger = minTotal;
+			this.updateHunger = hunger;
+		} else {
+			this.updateHunger = hunger;
+		}
+		
+		// create a variable that holds the previous value
+		int previousHunger = this.updateHunger;
+		
+		// when feed gets called, decrease hunger by random #
+		int feedHungerCalc = hunger - randomFeedHungerValue;
+		
+		// set updateValues to new values
+		newHunger = feedHungerCalc;
+		this.updateHunger = newHunger;
+				
+		//setting result variable
+		int resultFeedHunger = Math.max(newHunger, minTotal);
+		
+		// update default values to new values
+		hunger = newHunger;
+		
+		// --------------- ENERGY --------------- //
+
+		// create random energy array
+		int[] feedEnergy = {10, 18, 13, 8, 9, 15, 25};
+		
+		// create random object
+		Random randomFeedEnergy = new Random();
+		
+		// create variable that grabs a random # from the feedEnergy array
+		int randomFeedEnergyIndex = randomFeedEnergy.nextInt(feedEnergy.length);
+		
+		// set variable to equal the value of the random #
+		int randomFeedEnergyValue = feedEnergy[randomFeedEnergyIndex];
+		
+		// if previous values = minTotal, set previous values to minTotal, otherwise set previous values
+		if (energy <= minTotal) {
+			energy = minTotal;
+			this.updateEnergy = energy;
+		} else if (energy >= maxTotal) {
+			energy = maxTotal;
+			this.updateEnergy = energy;
+		} else {
+			this.updateEnergy = energy;
+		}
+		
+		// create a variable that holds the previous value
+		int previousEnergy = this.updateEnergy;
 		
 		// when feed gets called, decrease hunger by 15 and increase energy by 12
-		newHunger = hunger - 15;
-		newEnergy = energy + 12;
+		int newEnergyCalc = energy + randomFeedEnergyValue;
 		
-		// set updateValue to new value
-		int feedHunger = newHunger;
-		this.updateHunger = feedHunger;
+		// set updateValues to new values
+		newEnergy = newEnergyCalc;
+		this.updateEnergy = newEnergy;
 		
-		int feedEnergy = newEnergy;
-		this.updateEnergy = feedEnergy;
+		// update default values to new values
+		energy = newEnergy;
 		
 		System.out.println("you successfully fed " + name + "!");
 		
 		//setting result variable
-		int resultFeedHunger = Math.min(newHunger, maxTotal);
-		int resultFeedEnergy = Math.max(newEnergy, minTotal);
+		int resultFeedEnergy = Math.min(newEnergy, maxTotal);
 		
-		if ( feedHunger  >= maxTotal) {
+		
+		// if values hit maxTotal or minTotal, don't update any values, otherwise update values
+		if ( newHunger <= minTotal) {
 			System.out.println(name + " is too full! YOU NEED TO PLAY WITH IT");
+			// set hunger to minTotal
 			this.updateHunger = resultFeedHunger;
-			this.updateEnergy = energy;
-			System.out.println("Hunger Level: " + resultFeedHunger + ". Energy Level: " + this.updateEnergy);
-		} else if (feedEnergy <= minTotal) {
+			// set energy to previous value so it won't update
+			energy = previousEnergy;
+			System.out.println("Hunger Level: " + this.updateHunger + ". Energy Level: " + energy);
+		} else if (newEnergy >= maxTotal) {
 			System.out.println(name + " has too much energy! YOU NEED TO PLAY WITH IT");
-			this.updateHunger = hunger;
+			// set hunger to previous value so it won't update
+			hunger = previousHunger;
+			// set energy to maxTotal
 			this.updateEnergy = resultFeedEnergy;
-			System.out.println("Hunger Level: " + this.updateHunger + ". Energy Level: " + resultFeedEnergy);
+			System.out.println("Hunger Level: " + hunger + ". Energy Level: " + this.updateEnergy);
 		} else {
-			System.out.println("Hunger Level: " + this.updateHunger + ". Energy Level: " + this.updateEnergy);
+			System.out.println("Hunger Level: " + hunger + ". Energy Level: " + energy);
 		}
-		
 	}
 	
 	public void sleep() {
-		// create random number array or something between 1 and 4
-		// when user clicks sleep, it will draw random number
-		// random number 1: sleeps for 30 minutes, 2: sleeps for 1 hour, 3: sleeps for 1 hour 30 minutes, 4: sleeps for 2 hours.
-		// if 1 is selected: hunger will increase by 10, energy will increase by 10
-		// if 2 is selected: hunger will increase by 15, energy will increase by 15
-		// if 3 is selected: hunger will increase by 20, energy will increase by 20
-		// if 4 is selected: hunger will increase by 25, energy will increase by 25
+		
+		// create sleep array and call a random # from the array
+		// value is sleep in minutes
+		int[] sleep = {30, 45, 60, 75};
+		
+		// create random object
+		Random randomSleep = new Random();
+		
+		// create variable that grabs random # from the array
+		int randomSleepIndex = randomSleep.nextInt(sleep.length);
+
+		// set a variable to equal the random # that was called
+		int randomSleepTime = sleep[randomSleepIndex];
+		
+		// create sleepValues array
+		int[] sleepValue = {10, 15, 20, 25};
+		
+
+
+		// if 30 is selected: hunger will increase by 10, energy will increase by 10
+		// if 45 is selected: hunger will increase by 15, energy will increase by 15
+		// if 60 is selected: hunger will increase by 20, energy will increase by 20
+		// if 75 is selected: hunger will increase by 25, energy will increase by 25
+		if (randomSleepTime == sleep[0]) {
+			sleepIndex = sleepValue[0];
+		} else if (randomSleepTime == sleep[1]) {
+			sleepIndex = sleepValue[1];
+		} else if (randomSleepTime == sleep[2]) {
+			sleepIndex = sleepValue[2];
+		} else if (randomSleepTime == sleep[3]) {
+			sleepIndex = sleepValue[3];
+		}
+		
+		// --------------- HUNGER --------------- //
+		// if hunger is less than minTotal, set to minTotal
+		// if hunger is more than maxTotal, set to maxTotal
+		// otherwise set updateValue to previous values
+		if (hunger >= maxTotal) {
+			hunger = maxTotal;
+			this.updateHunger = hunger;
+		} else if (hunger <= minTotal) {
+			hunger = minTotal;
+			this.updateHunger = hunger;
+		} else {
+			this.updateHunger = hunger;
+		}
+		
+		// set previous to old value
+		int previousHunger = this.updateHunger;
+				
+		// set updateValue to a new value
+		int newHunger = sleepIndex + hunger;
+		this.updateHunger = newHunger; 
+				
+		// set default hunger value to the new value
+		hunger = newHunger;
+		
+		//setting result variable
+		int resultSleepHunger = Math.min(newHunger, maxTotal);
+		
+		// --------------- ENERGY --------------- //
+		// if energy is less than minTotal, set to minTotal
+		// if energy is more than maxTotal, set to maxTotal
+		// otherwise set updateValue to previous values
+		if (energy <= minTotal) {
+			energy = minTotal;
+			this.updateEnergy = energy;
+		} else if (energy >= maxTotal) {
+			energy = maxTotal;
+			this.updateEnergy = energy;
+		} else {
+			this.updateEnergy = energy;
+		}
+		
+		// previous value
+		int previousEnergy = this.updateEnergy;
+				
+		// set the updateValue to a new value
+		int newEnergy = sleepIndex + energy;
+		this.updateEnergy = newEnergy;
+		
+		// set default hunger value to the new value
+		energy = newEnergy;
+		
+		// output that states "play" has been called
+		System.out.println("You successfully put " + name + " to sleep for " + sleepIndex + " minutes");
+				
+		//setting result variable
+		int resultSleepEnergy = Math.min(newEnergy, maxTotal);
+		
+		// if hunger levels reach 100 or energy reaches 100, stop updating hunger and energy,
+		// tell the user to feed or play with their pet
+		if ( newHunger  >= maxTotal) {
+			System.out.println(name + " is starving! YOU NEED TO FEED IT");
+			// set updateHunger to maxTotal
+			this.updateHunger = resultSleepHunger;
+			// set energy to previous value so it doesn't update
+			energy = previousEnergy;
+			System.out.println("Hunger Level: " + this.updateHunger + ". Energy Level: " + previousEnergy);
+		} else if (newEnergy >= maxTotal) {
+			System.out.println(name + " is has too much energy! YOU NEED TO PLAY WITH IT");
+			// set hunger to previous value to it doesn't update
+			hunger = previousHunger;
+			// set energy to maxTotal
+			this.updateEnergy = resultSleepEnergy;
+			System.out.println("Hunger Level: " + previousHunger + ". Energy Level: " + this.updateEnergy);
+		} else {
+			System.out.println("Hunger Level: " + hunger + ". Energy Level: " + energy);
+		}
 	}
 	
-	
+	/*
+	 * MAYBE:
+	 * WHEN USER CREATES NEW DOG, LET THE USER KEEP PLAYING WITH THE SAME DOG
+	 * AFTER CREATING IT.
+	 * LET THE USER TYPE PLAY, FEED, OR SLEEP TO CALL THE RESPECTIVE METHODS.
+	 * IF USER WANTS TO QUIT THEY CAN TYPE ABANDON.
+	 */
 	public static void main(String[] args) {
 		// Test scanner
 		Scanner consoleScanner = new Scanner(System.in);
 		Dog test = new Dog(consoleScanner);
 		test.dogInfo();
 		test.play();
-		test.play();
-		test.play();
-		test.play();
-		test.play();
+		test.sleep();
+		test.sleep();
+		test.sleep();
+		test.sleep();
+		test.sleep();
+		test.feed();
+		test.feed();
 		test.play();
 	}
 
